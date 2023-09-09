8293aec51ae966e313ec297109f67121d73c7a41
Devin <Devin14892@hotmail.comM>
2023-09-09 18:26:18 -0400

    added two more for the fucntions test

---
+++ Lesser of two evens.py
@@ -0,0 +1,17 @@
+group = [(2,4), (2,5), (16,8), (21,7)]
+
+def lesser_of_two_events(set):
+    for x,y in set:
+        if x % 2 == 0 and y % 2 == 0:
+            if x > y:
+               print(y,' is smaller')
+            else:
+               print(x,' is smaller')
+        else:
+            if x > y:
+               print(x,' is larger')
+            else:
+               print(y,' larger')
+
+
+lesser_of_two_events(group)
