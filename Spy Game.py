bdb789ffad818cc156c7d3f29d7d23a888bc6739
Devin <Devin14892@hotmail.comM>
2023-09-09 18:52:32 -0400

    added spy game function challenge

---
+++ Spy Game.py
@@ -0,0 +1,27 @@
+
+list1 = ([1,2,4,0,0,7,5])
+list2 = ([1,0,2,4,0,5,7])
+list3 = ([1,7,2,0,4,5,0])
+
+
+def spy_game(numbers):
+    numberslist = [int(x) for x in numbers]
+    key = []
+    awn = [0,0,7]
+    for i in numberslist:
+        if i in [0, 7]:
+            key.append(i)
+    
+    if key == awn:
+        print('match found', key)
+
+
+
+
+
+    pass
+
+
+spy_game(list1)
+spy_game(list2)
+spy_game(list3)
